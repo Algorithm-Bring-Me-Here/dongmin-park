28f50849fa294f43cd41a5aaeb40efb73399d719
Dongmin <pdm001125@naver.com>
2024-05-06 17:09:48 +0900

    Lv.1 크레인 인형뽑기 게임 Solve

---
+++ Lv.1/크레인 인형뽑기 게임.kts
@@ -0,0 +1,25 @@
+class Solution {
+    fun solution(board: Array<IntArray>, moves: IntArray): Int {
+        val row = board.size
+        val col = board[0].size
+        val answer = mutableListOf<Int>()
+        var count = 0
+
+        for (m in moves) {
+            for (i in 0 until row) {
+                if (board[i][m - 1] != 0) {
+                    answer.add(board[i][m - 1])
+                    board[i][m - 1] = 0
+                    break
+                }
+            }
+
+            if (answer.size > 1 && answer[answer.size - 2] == answer[answer.size - 1]) {
+                answer.removeAt(answer.size - 1)
+                answer.removeAt(answer.size - 1)
+                count += 2
+            }
+        }
+        return count
+    }
+}
